1cd42293cabaf113341113e579f533f8da7962d1
jonathanbyrne <jonathan.byrne@yahoo.com>
2017-06-30 15:46:17 -0400

    Added partial classes to sample

---
+++ test/ByrneLabs.Commons.MetadataDom.Tests.SampleToParse/PartialSampleA.cs
@@ -0,0 +1,24 @@
+﻿namespace ByrneLabs.Commons.MetadataDom.Tests.SampleToParse
+{
+    internal partial class PartialSample1
+    {
+        public int Id { get; set; }
+    }
+
+    internal partial class PartialSample1
+    {
+        public string Name { get; set; }
+    }
+
+    internal partial class PartialSample2
+    {
+        public int Id { get; set; }
+    }
+
+    internal class PartialSample3
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ test/ByrneLabs.Commons.MetadataDom.Tests.SampleToParse/PartialSampleB.cs
@@ -0,0 +1,14 @@
+﻿namespace ByrneLabs.Commons.MetadataDom.Tests.SampleToParse
+{
+    internal partial class PartialSample2
+    {
+        public string Name { get; set; }
+    }
+
+    internal class PartialSample4
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
